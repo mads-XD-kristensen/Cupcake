ee5239e6a7e80125178117c647681a609dd41dbe
mads-XD-kristensen <mads-XD-kristensen@users.noreply.github.com>
2020-03-30 21:24:12 +0200

    connectoren er nu lavet til dropletten

--- Cupcake code/src/main/java/DBAccess/Connector.java
+++ Cupcake code/src/main/java/DBAccess/Connector.java
@@ -5,28 +5,44 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
- The purpose of Connector is to...
-
- @author kasper
+ * The purpose of Connector is to...
+ *
+ * @author kasper
  */
 public class Connector {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/cupcake";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "uqt42vqx";
+    private static String URL;
+    private static String USERNAME;
+    private static String PASSWORD;
 
     private static Connection singleton;
 
-    public static void setConnection( Connection con ) {
+    public static void setConnection(Connection con) {
         singleton = con;
     }
 
     public static Connection connection() throws ClassNotFoundException, SQLException {
-        if ( singleton == null ) {
-            Class.forName( "com.mysql.cj.jdbc.Driver" );
-            singleton = DriverManager.getConnection( URL, USERNAME, PASSWORD );
+        if (singleton == null) {
+            setDBCredentials();
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            singleton = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         }
         return singleton;
     }
 
+    public static void setDBCredentials() {
+        String deployed = System.getenv("DEPLOYED");
+
+        if (deployed != null) {
+            // Prod: hent variabler fra setenv.sh
+            URL = System.getenv("JDBC_CONNECTION_STRING");
+            USERNAME = System.getenv("JDBC_USER");
+            PASSWORD = System.getenv("JDBC_PASSWORD");
+        } else {
+            //LOCALHOST
+            URL = "jdbc:mysql://localhost:3306/cupcake";
+            USERNAME = "root";
+            PASSWORD = "uqt42vqx";
+        }
+    }
 }
